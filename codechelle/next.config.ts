406043339cab1a32236c217e61a53d2e3929df2e
codechelle <codechelle@users.noreply.github.com>
2025-08-13 00:49:29 -0400

    fix: make more additions

--- codechelle/next.config.ts
+++ codechelle/next.config.ts
@@ -1,17 +1,22 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
-module.exports = nextConfig
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
